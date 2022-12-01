e759652144f73ce6c8c89ade4440c0d9b5bf47fa
Dominik Ágh <agh.dominik@gmail.com>
2022-12-01 12:18:27 +0100

    Day 1 solutions in Python

---
+++ day-1/part_1.py
@@ -0,0 +1,20 @@
+INPUT_FILE = "input_1.txt"
+
+
+def solve():
+    max_calories = 0
+    current_calories = 0
+    with open(INPUT_FILE, "r+") as input_file:
+        for row in input_file:
+            row = row.strip("\n")
+            if row == "":
+                if current_calories >= max_calories:
+                    max_calories = current_calories
+                current_calories = 0
+            else:
+                current_calories += int(row)
+    print(max_calories)
+
+
+if __name__ == "__main__":
+    solve()
---
+++ day-1/part_2.py
@@ -0,0 +1,19 @@
+INPUT_FILE = "input_1.txt"
+
+
+def solve():
+    elves = []
+    current_calories = 0
+    with open(INPUT_FILE, "r+") as input_file:
+        for row in input_file:
+            row = row.strip("\n")
+            if row == "":
+                elves.append(current_calories)
+                current_calories = 0
+            else:
+                current_calories += int(row)
+    elves.sort(reverse=True)
+    print(elves[0]+elves[1]+elves[2])
+
+if __name__ == "__main__":
+    solve()
