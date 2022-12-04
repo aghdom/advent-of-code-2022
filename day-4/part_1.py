c878037b569320d41754a5905147e31e18f2ccb8
Dominik Ágh <agh.dominik@gmail.com>
2022-12-04 08:56:00 +0100

    Reformatted using black

--- day-4/part_1.py
+++ day-4/part_1.py
@@ -6,14 +6,15 @@ def solve():
     with open(INPUT_FILE, "r+") as input_file:
         for row in input_file:
             row = row.strip("\n")
-            ranges = row.split(',')
-            range_1 = ranges[0].split('-')
-            range_2 = ranges[1].split('-')
-            elf_1 = range(int(range_1[0]),int(range_1[1])+1)
-            elf_2 = range(int(range_2[0]),int(range_2[1])+1)
+            ranges = row.split(",")
+            range_1 = ranges[0].split("-")
+            range_2 = ranges[1].split("-")
+            elf_1 = range(int(range_1[0]), int(range_1[1]) + 1)
+            elf_2 = range(int(range_2[0]), int(range_2[1]) + 1)
             if set(elf_1) - set(elf_2) == set() or set(elf_2) - set(elf_1) == set():
                 count += 1
-    print(count) 
+    print(count)
+
 
 if __name__ == "__main__":
     solve()
