01a4e168b45159f43a447e9324119ea306733ba2
Dominik Ágh <agh.dominik@gmail.com>
2022-12-06 08:26:21 +0100

    Day 6 solutions in Python

---
+++ day-6/part_2.py
@@ -0,0 +1,17 @@
+INPUT_FILE = "input_6.txt"
+MARKER_LEN = 14
+
+
+def solve():
+    with open(INPUT_FILE, "r+") as input_file:
+        for row in input_file:
+            row = row.strip("\n")
+            for i in range(len(row) - MARKER_LEN):
+                uniq_chars = set(row[i : i + MARKER_LEN])
+                if len(uniq_chars) == MARKER_LEN:
+                    print(i + MARKER_LEN)
+                    return
+
+
+if __name__ == "__main__":
+    solve()
